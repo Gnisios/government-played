118d929fbac6e2c4cedde02ab26293fef77db0d3
Noltrox <31513344+Noltrox@users.noreply.github.com>
2019-06-08 19:01:17 +0200

    Update cl_init.lua

--- timeplayed/lua/timeplayed/cl_init.lua
+++ timeplayed/lua/timeplayed/cl_init.lua
@@ -1,3 +1,30 @@
-print ("Côté client chargé")
+local ply = LocalPlayer()
+
+
+
+net.Receive("startTimer", function()
+	timer.Create("minutesCP", 60, 0, function()
+		if file.Exists("serverplayers/"..ply:SteamID64()..".txt", "DATA") == false then
+				file.Write("serverplayers/"..ply:SteamID64()..".txt", "1")
+		else
+			file.Write("serverplayers/"..ply:SteamID64()..".txt", 
+				file.Read("serverplayers/"..ply:SteamID64()..".txt", "DATA") + 1)
+		end
+	end)
+end)
+
+
+
+net.Receive("removeTimer", function()
+	if timer.Exists("minutesCP") then
+		timer.Remove("minutesCP")
+	end
+end)
+
+net.Receive("plyDisconnect", function()
+	if timer.Exists("minutesCP") then
+		timer.Remove("minutesCP")
+	end
+end)
 
 
